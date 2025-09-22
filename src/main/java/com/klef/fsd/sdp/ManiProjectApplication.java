c4acd8aad5432bac7a5e9e236473b491fb06656d
Mavvvrickkkk <150039532+Mavvvrickkkk@users.noreply.github.com>
2025-09-22 22:18:28 +0530

    Update ManiProjectApplication.java

--- src/main/java/com/klef/fsd/sdp/ManiProjectApplication.java
+++ src/main/java/com/klef/fsd/sdp/ManiProjectApplication.java
@@ -2,14 +2,19 @@ package com.klef.fsd.sdp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // <-- ADD THIS IMPORT
 
 @SpringBootApplication
-public class ManiProjectApplication {
+public class ManiProjectApplication extends SpringBootServletInitializer { // <-- EXTEND THIS CLASS
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManiProjectApplication.class, args);
-
 		System.out.println("Mani tej Project is Runnning Successfully !!...");
 	}
-
+	
+	@Override // <-- ADD THIS ENTIRE METHOD
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ManiProjectApplication.class);
+    }
 }
